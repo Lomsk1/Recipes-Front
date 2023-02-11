85094231c9ee4b6f5890b22177e61f7a9bf5b58e
Lomsk1 <lomsianidzegiorgi123@gmail.com>
2023-02-11 19:04:11 +0400

    f-v

--- components/fridge/middle.tsx
+++ components/fridge/middle.tsx
@@ -3,10 +3,21 @@ import citrus from "../../assets/fruit/lemon-download-lime-splash-png-image-pngi
 import berries from "../../assets/fruit/m.png";
 import tropical from "../../assets/fruit/4-2-fruit-png-image.png";
 import apple from "../../assets/fruit/Apple.png";
+import bulbs from "../../assets/vegetable/163593-fresh-pic-onion-free-transparent-image-hd.png";
+import flowers_veg from "../../assets/vegetable/99315-images-green-broccoli-free-clipart-hd.png";
+import tomato from "../../assets/vegetable/3-2-tomato-transparent.png";
+import mushroom from "../../assets/vegetable/haridwar-mart-mushroom.png";
+import leaves from "../../assets/vegetable/154243-lettuce-green-free-download-png-hd.png";
+import rooted from "../../assets/vegetable/22147-8-carrot-cutting-pieces.png";
+import seeds from "../../assets/vegetable/png.monster-310.webp";
+import satacur from '../../assets/vegetable/39bb3615319143c2be16df1213daea5c.png'
+import potato from '../../assets/vegetable/pngimg.com - potato_PNG7082.png'
 
 function FridgeMiddleBody() {
   const imageWidth = 60;
-  const lineEightArr = [citrus, berries, tropical, apple];
+  const lineEightArr = [citrus, berries, tropical, apple, bulbs];
+  const lineSevenArr = [flowers_veg, tomato, mushroom, leaves, rooted];
+  const lineSixArr = [seeds, satacur, potato];
   return (
     <div className="middle">
       <div className="middle_ middle_1">
@@ -26,17 +37,40 @@ function FridgeMiddleBody() {
       </div>
       <div className="middle_ middle_6">
         <div className="right_side_line"></div>
+        {lineSixArr &&
+          lineSixArr.map((data, i) => (
+            <Image
+              key={i}
+              src={data}
+              alt=""
+              width={imageWidth}
+              height={imageWidth}
+              style={{ left: `${i * 70}px` }}
+            />
+          ))}
       </div>
       <div className="middle_ middle_7">
         <div className="right_side_line"></div>
+        {lineSevenArr &&
+          lineSevenArr.map((data, i) => (
+            <Image
+              key={i}
+              src={data}
+              alt=""
+              width={imageWidth}
+              height={imageWidth}
+              style={{ left: `${i * 70}px` }}
+            />
+          ))}
       </div>
       <div className="middle_ middle_8">
         <div className="right_side_line"></div>
         {lineEightArr &&
           lineEightArr.map((data, i) => (
             <Image
+              key={i}
               src={data}
-              alt="ციტრუსი"
+              alt=""
               width={imageWidth}
               height={imageWidth}
               style={{ left: `${i * 70}px` }}
